76717d1bc0629381839c9b9d0ad624873641de1b
siri-syl <sylee010101@gmail.com>
2021-11-12 09:58:50 +0900

    Fix: Update App.js, pages/NavBar.js, pages/Intro.js 제목의 세 가지 파일들을 수정했습니다.

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { Intro } from './pages/Intro';
+import  MainPage  from './pages/MainPage';
+import  LoginPage  from './pages/Login'
+import  MyPage  from './pages/MyPage';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { NavBar } from './pages/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <BrowserRouter>
+    <NavBar/>
+    <Switch>
+      <Route exact path="/">
+        <Intro/>
+      </Route>
+      <Route path="/mainpage">
+        <MainPage />
+      </Route>
+      <Route path="/mypage">
+        <MyPage />
+      </Route>
+      <Route path="/loginpage">
+        <LoginPage />
+      </Route>
+    </Switch>
+    </BrowserRouter>
+    </>
   );
 }
 
