9bce5d2432b36a8b3a29b51abc38a05f6e46f644
ksg9482 <ksg930523@gmail.com>
2021-11-12 09:40:43 +0900

    21/11/12morning

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from 'react';
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
+import NavBar from './component/NavBar';
+import LoginPage from './pages/Login';
+import styled from 'styled-components'
+
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar />
+      <div>
+        <Switch>
+          <Route  path='/' component={LoginPage} />
+        </Switch>
+      </div>
+    </Router>
+  
   );
 }
 
---
+++ client/src/component/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function NavBar () {
+
+
+return (
+    <div>
+        <div className='test'>
+            네비게이션바
+        </div>
+    </div>
+)
+
+}
