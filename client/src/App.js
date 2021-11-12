aaeaae47bc43fdccd8284c3d7050f725767889ff
baini <83062886+ksg9482@users.noreply.github.com>
2021-11-12 10:05:24 +0900

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,1 +1,24 @@
+import React from 'react';
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
+import NavBar from './component/NavBar';
+import LoginPage from './pages/Login';
+import styled from 'styled-components'
 
+
+function App() {
+
+
+  return (
+    <Router>
+      <NavBar />
+      <div>
+        <Switch>
+          <Route  path='/' component={LoginPage} />
+        </Switch>
+      </div>
+    </Router>
+  
+  );
+}
+
+export default App;
