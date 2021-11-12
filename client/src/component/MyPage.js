5b171c05f900e4aa5bae1d30eb6510a499f85148
ksg9482 <ksg930523@gmail.com>
2021-11-12 16:10:10 +0900

    add login page

--- client/src/component/MyPage.js
+++ client/src/component/MyPage.js
@@ -1,1 +1,18 @@
 //마이페이지 구현. 정보수정창 겸-> 회원탈퇴 모달창
+import React from 'react';
+import { useDispatch, useSelector } from "react-redux";
+
+
+
+export default function MyPage(props) {
+    
+const LoginState = useSelector(state=>state.LoginReducer);
+const {email} = LoginState.user
+console.log(email)
+
+    return (
+        <div>{email ? email:'데이터 들어오지 않았음'}</div>
+        
+        
+    )
+}
--- client/src/reducers/InitialState.js
+++ client/src/reducers/InitialState.js
@@ -1,4 +1,6 @@
 export const initialState = {
     user: {},
     isLogin: false,
+    email: '',
+    password: ''
   };
